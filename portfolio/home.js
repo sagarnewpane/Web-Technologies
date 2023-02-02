d69c625b2c5e5bd9e85847969036e06d9decc3c6
Sagar Newpane <sdreamnewpane@gmail.com>
2023-02-02 18:23:29 +0545

    added test

--- portfolio/home.js
+++ portfolio/home.js
@@ -4,11 +4,13 @@ element[0].addEventListener("mouseenter", function () {
   // code to execute when the cursor enters the element
   document.getElementsByClassName('logo')[0].style.color = 'rgb(235, 50, 17)';
   element[0].style.color = 'white';
+  element[0].style.transition = 'all 0.4s ease 0s';
   
 });
 element[0].addEventListener("mouseleave", function() {
   // code to execute when the cursor leaves the element
   document.getElementsByClassName('logo')[0].style.color = 'white';
   element[0].style.color = 'rgb(235, 50, 17)';
+  element[0].style.transition = 'all 0.4s ease 0s';
   
 });
