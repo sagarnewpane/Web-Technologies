aa2d0da378671dce014c462ccbbbce9224c6e199
Sagar Newpane <sdreamnewpane@gmail.com>
2023-02-07 01:17:46 +0545

    made visually appealing and some effects

--- portfolio/js/contact.js
+++ portfolio/js/contact.js
@@ -13,6 +13,7 @@ for(let i = 0; i < links.length; i++) {
         linkLogo.style.display = "block";
         if(id === "github") {
             img.src = "images/githubwhite.svg";
+            img.id = 'gihub';
             click_msg.innerHTML = `${text} visit the Github Profile.`;
             a.href = "https://github.com/sagarnewpane";
             a.target = "blank";
@@ -20,22 +21,43 @@ for(let i = 0; i < links.length; i++) {
 
         } else if(id === "linkedin") {
             img.src = "images/linkedin.svg";
+            img.id = 'linkedin';
             click_msg.innerHTML = `${text} visit the LinkedIn Profile.`;
-            a.href = "www.linkedin.com/in/sagarnewpane";
+            a.href = "https://www.linkedin.com/in/sagarnewpane";
             a.target = "blank";
             custom_text.innerHTML = `${text2} search the username - sagarnewpane`;
         }else if(id === "whatsapp") {
             img.src = "images/whatsapp.png";
+            img.id = 'whatsapp';
             click_msg.innerHTML = `${text} chat on WhatsApp.`;
             a.href = "https://wa.me/9779815911506";
             a.target = "blank";
             custom_text.innerHTML = `${text2} add the contact number - +977 9815911506`;
         }else if(id === "email") {
             img.src = "images/gmail.png";
+            img.id = 'email';
             click_msg.innerHTML = `${text} mail me.`;
             a.href = "mailto:sdreamnepal@gmail.com"
             a.target = "blank";
             custom_text.innerHTML = `${text2} mail me at - sreamnepal@gmail.com`;
         }
+        else if(id === "phone") {
+            img.src = "images/phone.png";
+            img.id = 'phone';
+            a.href ='#';
+            click_msg.innerHTML = `${text} copy phone number.`;
+            custom_text.innerHTML = `${text2} call me at - 9815911506`;
+        }
     });
 }
+
+// copies phone number
+let phone = document.querySelector('img');
+phone.addEventListener("click",function(){
+    if(this.getAttribute("id")=='phone'){
+        console.log('Clicked');
+        const num = '9815911506';
+        alert('Phone number copied.')
+        navigator.clipboard.writeText(num);
+    }
+});
